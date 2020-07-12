2d7ba8ef1ed1a64277b34fdcd174cd00abf80733
Mario Matos <mariomatos889@gmail.com>
2020-07-12 10:45:41 -0500

    completed introductory model

--- digitpredictor.py
+++ digitpredictor.py
@@ -1,3 +1,52 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
 
-def predict_images():
-	pass
+num_classes = 10
+input_shape = (28, 28, 1)
+batch_size = 128
+epochs = 20
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+
+# check what this does
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+
+print("===============================")
+print("x_train.shape: ", x_train.shape)
+print(x_train.shape[0], " train samples")
+print(x_test.shape[0], " test samples")
+print("===============================")
+
+# check what this produces
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# build convolutional neural network
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape, name="image_input"),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax")
+    ]
+)
+
+print("===============================")
+model.summary()
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+score = model.evaluate(x_test, y_test, verbose=1)
+
+print("Test loss: %s\nTest accuracy: %s" % (score[0], score[1]))
